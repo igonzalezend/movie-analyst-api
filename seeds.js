9ada8b9369fc5d89f79259dacba7e10945cd5d80
Ivan Gonzalez <Ivan.Gonzalez@endava.com>
2019-02-13 15:53:15 -0500

    Seeds modified

--- seeds.js
+++ seeds.js
@@ -5,10 +5,10 @@ async function main () {
   try {
     const pool = mysql.createPool({
       connectionLimit: 1000,
-      host: process.env.DB_HOST || 'igonzalezdb.cm6hb8mfglop.us-east-1.rds.amazonaws.com',
-      user: process.env.DB_USER || 'IGonzalez',
-      password: process.env.DB_PASS || 'Crackpokemon94!',
-      database: process.env.DB_NAME || 'RampUp'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
     })
     pool.query = util.promisify(pool.query)
 
