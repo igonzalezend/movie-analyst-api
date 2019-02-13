069ad52f8c3224c5308c542449e0ebf9b9b855fb
Ivan Gonzalez <Ivan.Gonzalez@endava.com>
2019-02-13 17:04:16 -0500

    Jenkinsfile modified and ecosystem modified

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
       name: "Movies_API",
       script: "./server.js",
       env: {
+        NODE_ENV: "development",
         DB_HOST: "igonzalezdb.cm6hb8mfglop.us-east-1.rds.amazonaws.com",
         DB_USER: " IGonzalez",
         DB_PASS: "Crackpokemon94!",
