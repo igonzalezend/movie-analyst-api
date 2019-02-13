b95f9b542b6b11c599dcfd9f5c0546afbe020a3e
Ivan Gonzalez <Ivan.Gonzalez@endava.com>
2019-02-13 15:47:43 -0500

    Seeds and ecosystem modified

--- ecosystem.config,.js
+++ ecosystem.config,.js
@@ -4,7 +4,7 @@ module.exports = {
       script: "./app.js",
       env: {
         DB_HOST: "igonzalezdb.cm6hb8mfglop.us-east-1.rds.amazonaws.com",
-        DB_USER: " Igonzalez",
+        DB_USER: " IGonzalez",
         DB_PASS: "Crackpokemon94!",
         DB_NAME: "RampUp",
         PORT: "3000",
--- seeds.js
+++ seeds.js
@@ -7,7 +7,7 @@ async function main () {
       connectionLimit: 1000,
       host: process.env.DB_HOST || 'igonzalezdb.cm6hb8mfglop.us-east-1.rds.amazonaws.com',
       user: process.env.DB_USER || 'IGonzalez',
-      password: process.env.DB_PASS || 'process.env.DB_PASS',
+      password: process.env.DB_PASS || 'Crackpokemon94!',
       database: process.env.DB_NAME || 'RampUp'
     })
     pool.query = util.promisify(pool.query)
