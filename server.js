f617052a7dc42f0e65e82d9071dd9f19d66c5572
Ivan Gonzalez <Ivan.Gonzalez@endava.com>
2019-02-14 16:08:54 -0500

    Changes have been made

--- server.js
+++ server.js
@@ -26,6 +26,19 @@ app.get('/movies', async function (req, res) {
   }
 })
 
+app.get('/mo', async function (req, res) {
+  try {
+    const rows = await pool.query(
+      'select m.title, m.release_year, m.score, r.name as reviewer, p.name as publication from Movies m,' +
+      'Reviewers r, Publications p where r.publication=p.name and m.reviewer=r.name'
+    )
+    res.json(rows)
+  } catch (err) {
+    console.error('API Error:', err)
+    res.staus(500).send({'msg': 'Internal server error'})
+  }
+})
+
 app.get('/reviewers', async function (req, res) {
   try {
     const rows = await pool.query('select r.name, r.publication, r.avatar from Reviewers r')
