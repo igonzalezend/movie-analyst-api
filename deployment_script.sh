31653fe64ca792f091d2dff22bb8bf0e8061d46a
Ivan Gonzalez <Ivan.Gonzalez@endava.com>
2019-03-14 11:31:22 -0500

    fixes

--- deployment_script.sh
+++ deployment_script.sh
@@ -2,6 +2,7 @@
 SERVICE_NAME="IGonzalez_ECS_Backend_Service"
 BUILD_NUMBER=$1
 TASK_FAMILY="IGonzalez_ECS_TD_Backend"
+CLUSTER="IGonzalez_ECS_Cluster"
 
 # Create a new task definition for this build
 sed -i "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" backend_service.json 
@@ -10,4 +11,6 @@ aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file:/
 # Update the service with the new task definition and desired count
 TASK_REVISION=`aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 
-aws ecs update-service --cluster IGonzalez_ECS_Cluster --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count 2
+aws ecs stop-task --cluster ${CLUSTER} --task $(aws ecs list-tasks --cluster ${CLUSTER} --service ${SERVICE_NAME} --output text --region us-east-1 --query taskArns[0])
+
+aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count 2
